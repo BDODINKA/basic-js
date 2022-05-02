20cc4ab9e99fb1372596f4ec296312e85d533cce
dmitry <bdodinka1992@gmail.com>
2022-05-02 22:05:41 +0300

    feat: add 75% finished

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix ) {
+    let cats = 0
+  for (massCat  of matrix){
+    for (str of massCat){
+      if (str === '^^' ){
+        cats++
+      }
+    }
+
+  }
+  return cats
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(str, options ) {
-//
-}
+function repeater(str, options) {
+  const strStr = String(str);
+  let addSep = "|";
+  let sep = "+";
+  let add = "";
 
+    if (options.additionSeparator === 0 || options.additionSeparator !== undefined){
+    addSep = options.additionSeparator;
+    }
+    if (options.separator === 0 || options.separator !== undefined){
+    sep = options.separator;
+    }
+    if (options.addition === 0 || options.addition !== undefined){
+    add = String(options.addition);
+    }
+
+
+  const repeat = strStr + (add + addSep).repeat(options.additionRepeatTimes - 1) + add;
+  const end = (repeat + sep).repeat(options.repeatTimes - 1) + repeat;
+
+  return end;
+}
 module.exports = {
   repeater
 };
